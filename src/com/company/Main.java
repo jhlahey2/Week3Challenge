4727134d0178ba5fefd4091ed9ae4115671b0c8f
GBTC440004UR <jhlahey@proton.com>
2018-10-05 09:15:37 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * Assignment
+ * You will write an application to print out the multiples of 5 and 7 in reverse order.
+ *
+ *  Your user will enter a number, and your application will print out all the required multiples from that number.
+ *
+ *  Once you are done, ask the user if they would like to enter another number, and continue until they choose to end the application.
+ */
+public class Main {
+
+    public static void main(String[] args)
+    {
+
+    }
+}
